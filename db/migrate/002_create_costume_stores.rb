566adc207b68a8beb10a5c5a78ecd4c663613c40
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-18 20:40:52 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -2,7 +2,13 @@
 class CreateCostumeStores < ActiveRecord::Migration[5.2]
   def change
     create_table :costume_stores do |t|
-      
+      t.string :name
+      t.string :location
+      t.integer :costume_integer
+      t.integer :employee_count
+      t.boolean :still_in_business
+      t.datetime :opening_time
+      t.datetime :closing_time
 
       t.timestamps, null: false
     end
