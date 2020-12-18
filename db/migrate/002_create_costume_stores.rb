588ca31fb80667d50fbc2633ff996489275ab395
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-18 20:39:20 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,13 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[5.2]
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.float :price
+      t.string :image_url
+      t.string :size
+
+      t.timestamps, null: false
+    end
+  end
+end
