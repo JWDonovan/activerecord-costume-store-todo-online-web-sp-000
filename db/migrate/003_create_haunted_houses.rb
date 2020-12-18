39ff661d68348f6ab5f34527fa273c3d62ec8bde
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-18 20:42:04 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,13 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.float :price
+      t.string :image_url
+      t.string :size
+
+      t.timestamps, null: false
+    end
+  end
+end
